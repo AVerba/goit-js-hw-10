1ab4f058f37a0e79ecc67d5e12bc3b34a1700836
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-07 13:48:39 +0200

    begining mod task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
