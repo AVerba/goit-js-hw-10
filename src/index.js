63ed0cfdbe7c9a445cee3fbc8051ab48c003b4c9
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-08 12:17:19 +0200

    added handler

--- src/index.js
+++ src/index.js
@@ -4,13 +4,32 @@ import debounce from 'lodash.debounce';
 
 import { getCountriesAPI } from './js/api/countriesApi';
 
+const ref={
+    inputRef: document.querySelector('#search-box'),
+    countryListRef: document.querySelector('.country-list'),
+    countryInfoRef: document.querySelector('.country-info'),
+}
 
 const DEBOUNCE_DELAY = 300;
-// const data=getCountriesAPI('usa');
+
+const inputHandler=(e)=>{
+    const inputValue=e.target.value.trim();
+
+    getCountriesAPI(inputValue).then(data=>{
+        if(data.length>1){
+            data.forEach(item=>{
+                const {name}=item;
+                console.log(name.official)
+
+            })
+            return;
+        }
+        const {capital,name, population, languages,flags}=data;
+        // console.dir(`${name}, ${capital}, ${population}, ${languages}, ${flags}`)
+  
+     console.log(length)
+    });
+}
 
 
-getCountriesAPI('usa').then(data=>{
-    // const amount = data.length;
-    const {length} =data;
- console.log(length)
-});
+ref.inputRef.addEventListener('input',debounce(inputHandler,DEBOUNCE_DELAY))
