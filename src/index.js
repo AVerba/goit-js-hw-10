fda89bec650437ef777a733cfaa7897e313119ef
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-13 13:50:45 +0200

    added app.js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
 import './sass/main.scss';
-import debounce from 'lodash.debounce';
-
-import { clearContent } from "./js/ClearContent";
-import { renderData } from "./js/renderData";
-
-
-const ref={
-    inputRef: document.querySelector('#search-box'),
-    countryListRef: document.querySelector('.country-list'),
-    countryInfoRef: document.querySelector('.country-info'),
-}
-
-const DEBOUNCE_DELAY = 300;
-
-const inputHandler=(e)=>{
-    const inputValue=e.target.value.trim();
-    clearContent(ref.countryListRef);
-    clearContent(ref.countryInfoRef);
-    renderData(inputValue,ref);
-}
-
-ref.inputRef.addEventListener('input',debounce(inputHandler,DEBOUNCE_DELAY))
+import './js/App';
---
+++ src/js/App.js
@@ -0,0 +1,22 @@
+import debounce from 'lodash.debounce';
+
+import { clearContent } from "./ClearContent";
+import { renderData } from "./renderData";
+
+
+const ref={
+  inputRef: document.querySelector('#search-box'),
+  countryListRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+}
+
+const DEBOUNCE_DELAY = 300;
+
+const inputHandler=(e)=>{
+  const inputValue=e.target.value.trim();
+  clearContent(ref.countryListRef);
+  clearContent(ref.countryInfoRef);
+  renderData(inputValue,ref);
+}
+
+ref.inputRef.addEventListener('input',debounce(inputHandler,DEBOUNCE_DELAY))
